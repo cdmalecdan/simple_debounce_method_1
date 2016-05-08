573f887bb18dcf4656afbb04103648f649189c99
layadcircuits <cdmalecdan@gmail.com>
2016-05-08 14:56:55 +0800

    Added files via upload

---
+++ simple_debounce_method_1.ino
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 (C) layadcircuits.com
+ * Project: 
+ * A simple approach to switch debouncing via software using short delays and multiple input checks
+ * For Questions/Comments: chris@layadcircuits.com
+ * Revision History:
+ * 1.0 - initial commit
+ * Notes: 
+ * We assume a PBNO switch connected at pin 2. When the button is pressed and debounced correctly, 
+ * we display the number of times the button was pressed
+ */
+const byte BUTTON = 2;
+const byte LED = 13;
+boolean pressed;
+unsigned int count;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUTTON,INPUT_PULLUP);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(BUTTON)==LOW){
+    delay(1);
+    if(digitalRead(BUTTON)==LOW){
+      delay(1);
+      if(digitalRead(BUTTON)==LOW){
+        delay(1);
+        if(digitalRead(BUTTON)==LOW){
+          delay(1);
+          if(digitalRead(BUTTON)==LOW){
+            delay(1);
+            if(digitalRead(BUTTON)==LOW){
+              count++;
+              Serial.print("Number of times button was pressed = ");
+              Serial.println(count);
+            }
+          }
+        } 
+      }
+    }
+  }
+  
+}
